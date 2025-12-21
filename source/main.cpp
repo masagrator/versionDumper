367d1c74d8ad54c9d61bafd902888620582d2d91
MasaGratoR <masagrator@gmail.com>
2025-12-21 16:26:07 +0100

    Add files via upload

--- source/main.cpp
+++ source/main.cpp
@@ -2,16 +2,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <string>
 #include <map>
+#include <curl/curl.h>
 
-void Compare() {
+constexpr char privateKey[] = {
+#embed "privateKey.txt"
+};
+
+bool screenTurnedOff = false;
+
+void userAppExit(void) {
+    if (screenTurnedOff == false) return;
+    appletSetMediaPlaybackState(false);
+    lblInitialize();
+    lblSwitchBacklightOn(1000);
+    lblExit();
+}
+
+bool Compare() {
     std::map<int64_t, int32_t> old_entries;
     std::map<int64_t, int32_t> new_entries;
 
     FILE* file = fopen("sdmc:/version_dump_temp.txt", "r");
     if (!file) {
         printf("Couldn't find old file!\n");
-        return;
+        return false;
     }
     char text[64];
     fgets(text, 64, file);
@@ -24,10 +40,11 @@ void Compare() {
     file = fopen("sdmc:/version_dump.txt", "r");
     if (!file) {
         printf("Couldn't find new file!\n");
-        return;
+        return false;
     }
     fgets(text, 64, file);
     size_t found = 0;
+    size_t added = 0;
     while (fgets(text, 64, file)) {
         int64_t titleid = std::strtoll(&text[0], nullptr, 16);
         int32_t version = std::strtol(&text[50], nullptr, 10);
@@ -45,10 +62,14 @@ void Compare() {
                 consoleUpdate(NULL);
             }
         }
-        else printf("%016lX was added!\n", titleid);
+        else {
+            printf("%016lX was added!\n", titleid);
+            added++;
+        }
     }
     fclose(file);
     if (found == 0) printf("No new changes in updates were found!\n");
+    return (found > 0) || (added > 0);
 }
 
 void Test() {
@@ -74,8 +95,142 @@ void Test() {
     }
     else printf(CONSOLE_RED "File creation failed!\n" CONSOLE_RESET);
     delete[] entries;
+}
 
+// Callback to handle response data
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* userp) {
+    userp->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+void SendIt() {
+    if (R_FAILED(nifmInitialize(NifmServiceType_User))) {
+        printf("No internet connection detected!\n");
+        return;
+    }
+	u32 dummy = 0;
+	NifmInternetConnectionType NifmConnectionType = (NifmInternetConnectionType)-1;
+	NifmInternetConnectionStatus NifmConnectionStatus = (NifmInternetConnectionStatus)-1;
+	Result rc = nifmGetInternetConnectionStatus(&NifmConnectionType, &dummy, &NifmConnectionStatus);
+    nifmExit();
+    if (R_FAILED(rc) || NifmConnectionStatus != NifmInternetConnectionStatus_Connected) {
+        printf("No internet connection detected!\n");
+        return;
+    }
+    socketInitializeDefault();
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    CURL* curl = curl_easy_init();
+    if (!curl) {
+        printf("Curl Easy Init error!\n");
+        curl_global_cleanup();
+        socketExit();
+        return;
+    }
+    FILE* file = fopen("sdmc:/version_dump.txt", "r");
+    fseek(file, 0, 2);
+    auto size = ftell(file);
+    fseek(file, 0, 0);
+    char* dump = (char*)calloc(1, size+1);
+    fread(dump, 1, size, file);
+    fclose(file);
+
+    std::string file_content = dump;
+    free(dump);
+
+    // Base64 encode the content
+    std::string encoded_content;
+    static const char* base64_chars = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+    int val = 0;
+    int valb = 0;
+    for (unsigned char c : file_content) {
+        val = (val << 8) + c;
+        valb += 8;
+        while (valb >= 6) {
+            valb -= 6;
+            encoded_content.push_back(base64_chars[(val >> valb) & 0x3F]);
+        }
+    }
+    if (valb > 0) {
+        encoded_content.push_back(base64_chars[(val << (6 - valb)) & 0x3F]);
+    }
+    while (encoded_content.size() % 4) {
+        encoded_content.push_back('=');
+    }
     
+    // Build API URL
+    std::string url = "https://api.github.com/repos/masagrator/version_dump/contents/version_dump.txt";
+    std::string auth = "Authorization: token ";
+    auth += privateKey;
+
+    std::string response;
+    std::string file_sha;
+
+    curl_slist* headers_get = nullptr;
+    headers_get = curl_slist_append(headers_get, "User-Agent: libcurl");
+    headers_get = curl_slist_append(headers_get, auth.c_str());
+
+    std::string get_response;
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers_get);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &get_response);
+
+    curl_easy_perform(curl);
+    long get_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &get_code);
+
+    if (get_code == 200) {
+        size_t sha_pos = get_response.find("\"sha\"");
+        if (sha_pos != std::string::npos) {
+            size_t start = get_response.find("\"", sha_pos + 6) + 1;
+            size_t end = get_response.find("\"", start);
+            file_sha = get_response.substr(start, end - start);
+            printf("Found existing file SHA: %s\n", file_sha.c_str());
+        }
+    }
+
+    curl_slist_free_all(headers_get);
+
+    // Create JSON payload
+    std::string json_payload = R"({"message":"Update version_dump.txt","content":")" 
+            + encoded_content + R"(","sha":")" + file_sha + R"(","branch":")" + "main" + R"("})";
+
+    curl_slist* headers = nullptr;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    headers = curl_slist_append(headers, auth.c_str());
+    headers = curl_slist_append(headers, "User-Agent: libcurl");
+
+    curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_payload.c_str());
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PUT");
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &response);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
+    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
+
+    // Perform request
+    CURLcode res = curl_easy_perform(curl);
+
+    long http_code = 0;
+    curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &http_code);
+
+    curl_slist_free_all(headers);
+    curl_easy_cleanup(curl);
+    curl_global_cleanup();
+    socketExit();
+
+    if (res != CURLE_OK) {
+        printf("CURLE NOT OK! %d\n", res);
+    }
+    else if (http_code != 201 && http_code != 200) {
+        printf("HTTP CODE WRONG! %ld\n", http_code);
+        printf("%s\n", response.c_str());
+    }
+    else printf("SENDING SUCCESSFUL!\n");
+    return;
 }
 
 // Main program entrypoint
@@ -107,6 +262,10 @@ int main(int argc, char* argv[])
     }
     printf("Press X to dump.\n");
     printf("Press + to exit.\n");
+    if (old_file_available) {
+        printf("Press Y to send it to github.\n");
+    }
+    printf("Press - to do automatic scan every 5 minute with turned off screen. No way to exit it without pressing HOME menu.\n");
 
     consoleUpdate(NULL);
 
@@ -132,11 +291,30 @@ int main(int argc, char* argv[])
                 printf("\n---\n\n");
             }
             printf("File was saved to sdmc:/version_dump.txt\n");
+            printf("Press Y to send it to github.\n");
             printf("Press + to exit.\n");
         }
 
         if (kDown & HidNpadButton_Plus)
             break; // break in order to return to hbmenu
+
+        if (old_file_available && (kDown & HidNpadButton_Y))
+            SendIt();
+
+        if (kDown & HidNpadButton_Minus) {
+            appletSetMediaPlaybackState(true);
+            lblInitialize();
+            lblSwitchBacklightOff(1000);
+            lblExit();
+            screenTurnedOff = true;
+            while(true) {
+                svcSleepThread(5llu * 60 * 1000 * 1000 * 1000);
+                Test();
+                if (Compare()) {
+                    SendIt();
+                }
+            }
+        }
 
         // Update the console, sending a new frame to the display
         consoleUpdate(NULL);
